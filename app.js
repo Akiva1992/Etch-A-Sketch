fbf9ee796951303655fb0316950b84a11b42d048
Akiva1992 <breuerakiva@gmail.com>
2022-08-27 23:37:08 +0300

    basic functions working

---
+++ app.js
@@ -0,0 +1,46 @@
+let container = document.querySelector(".container");
+
+let num = 16;
+
+for (let i =0; i<num; i++){
+    for (let j=0; j<num; j++){
+        container.appendChild(document.createElement("div")).classList.add("pixel");
+
+    }
+}
+
+
+let divWidth = 100 / num;
+console.log(divWidth)
+
+const divs = document.querySelectorAll('.pixel');
+const arr = Array.from(divs);
+
+arr.forEach(div => {
+    div.style.width =`${divWidth}%`
+    let d = div.style.height =`${divWidth}%`
+    console.log(d)
+
+});
+
+const body = document.getElementsByTagName("body")[0];
+
+let isDown = false;
+
+body.addEventListener("mouseup",() =>{
+    isDown = false;
+});
+
+body.addEventListener("mousedown",() =>{
+    isDown = true;
+});
+
+
+arr.forEach(div => {
+    div.addEventListener("mouseover",() =>{
+        if (isDown == true){
+            div.classList.add("color");
+        }
+    });
+});
+

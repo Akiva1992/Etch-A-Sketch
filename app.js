936385f65bef4631c62a7769d4dc4eed822d26c5
Akiva1992 <breuerakiva@gmail.com>
2022-08-29 11:43:07 +0300

    Did all the mark-up and element adding

--- app.js
+++ app.js
@@ -1,46 +1,45 @@
-let container = document.querySelector(".container");
+// let sketchPad = document.querySelector(".sketch-pad");
 
-let num = 16;
+// let num = 16;
 
-for (let i =0; i<num; i++){
-    for (let j=0; j<num; j++){
-        container.appendChild(document.createElement("div")).classList.add("pixel");
-
-    }
-}
-
-
-let divWidth = 100 / num;
-console.log(divWidth)
-
-const divs = document.querySelectorAll('.pixel');
-const arr = Array.from(divs);
-
-arr.forEach(div => {
-    div.style.width =`${divWidth}%`
-    let d = div.style.height =`${divWidth}%`
-    console.log(d)
-
-});
-
-const body = document.getElementsByTagName("body")[0];
-
-let isDown = false;
-
-body.addEventListener("mouseup",() =>{
-    isDown = false;
-});
-
-body.addEventListener("mousedown",() =>{
-    isDown = true;
-});
+// for (let i =0; i<num; i++){
+//     for (let j=0; j<num; j++){
+//         sketchPad.appendChild(document.createElement("div")).classList.add("pixel");
+//     }
+// }
 
 
-arr.forEach(div => {
-    div.addEventListener("mouseover",() =>{
-        if (isDown == true){
-            div.classList.add("color");
-        }
-    });
-});
+// let divWidth = 100 / num;
+// console.log(divWidth)
+
+// const divs = document.querySelectorAll('.pixel');
+// const arr = Array.from(divs);
+
+// arr.forEach(div => {
+//     div.style.width =`${divWidth}%`
+//     let d = div.style.height =`${divWidth}%`
+//     console.log(d)
+
+// });
+
+// const body = document.getElementsByTagName("body")[0];
+
+// let isDown = false;
+
+// body.addEventListener("mouseup",() =>{
+//     isDown = false;
+// });
+
+// body.addEventListener("mousedown",() =>{
+//     isDown = true;
+// });
+
+
+// arr.forEach(div => {
+//     div.addEventListener("mouseover",() =>{
+//         if (isDown == true){
+//             div.classList.add("color");
+//         }
+//     });
+// });
 
